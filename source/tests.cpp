6c52ca14b0aa79c4a0cae6b2af8bd4e33ef06dab
Sabbelz <Sebastian.Reichmann@uni-weimar.de>
2019-04-17 17:03:07 +0200

    task 2.1 & 2.2

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,6 +1,16 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include "vec2.hpp"
 
+
+TEST_CASE("niy", "[niy]"){
+  Vec2 a;
+  Vec2 b{5.1f,-9.3f};
+  REQUIRE(a.x == 0.0f);
+  REQUIRE(a.y == 0.0f);
+  REQUIRE(b.x == 5.1f);
+  REQUIRE(b.y == -9.3f);
+}
 int main(int argc, char *argv[])
 {
   return Catch::Session().run(argc, argv);
